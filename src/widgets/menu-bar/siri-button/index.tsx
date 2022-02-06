ab6f0e274b300d02b6e4023175f24805d1c37da7
Uurtsaikh <vvrtsaix.n@gmail.com>
2022-02-06 23:38:56 +0800

    Added siri button & modal

---
+++ src/widgets/menu-bar/siri-button/index.tsx
@@ -0,0 +1,65 @@
+import Image from 'next/image'
+import React, { FC, useEffect, useRef, useState } from 'react'
+import { toast } from 'react-toastify'
+
+interface Props {
+  size?: number
+}
+
+const SiriButton: FC<Props> = ({ size = 24 }) => {
+  const video = useRef<HTMLVideoElement>(null)
+
+  return (
+    <button
+      className="h-6 rounded px-3 align-middle focus:bg-[#ffffff30] active:bg-[#ffffff30]"
+      onClick={() => {
+        video.current?.play()
+        toast(
+          <div className="flex flex-col items-center justify-center gap-2 p-1 text-sm text-white">
+            <SiriText />
+            <video
+              loop
+              autoPlay
+              width={48}
+              height={48}
+              className="rounded-full"
+              poster="images/siri-orb.png"
+            >
+              <source src="videos/siri-orb.mp4" type="video/mp4" />
+            </video>
+          </div>,
+          {
+            position: toast.POSITION.TOP_RIGHT,
+            role: 'siri',
+            className: 'top-8 right-2 bg-slate-700/80',
+            autoClose: 8000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+          }
+        )
+      }}
+    >
+      <Image
+        width={size}
+        height={size}
+        src="/images/siri-orb.png"
+        alt="siri-orb"
+      />
+    </button>
+  )
+}
+
+const SiriText: FC = () => {
+  const [text, setText] = useState('What can I help you with?')
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setText("Go ahead. I'm listening...")
+    }, 2000)
+    return () => clearTimeout(timeOut)
+  }, [])
+  return <span>{text}</span>
+}
+
+export default SiriButton
