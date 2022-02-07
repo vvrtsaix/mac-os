79f0ec410ebb401ae3c12846dfc0a05ab7fbe011
vvrtsaix <43521715+vvrtsaix@users.noreply.github.com>
2022-02-07 05:20:40 +0000

    Deploying to gh-pages from @ vvrtsaix/mac-os@4ea1b91a1a0ef47ece4b2720d612e51f0c9ce7cd 🚀

--- _next/static/4xJQGPPN0tfS5S9nj-PN9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/527-2f3c84121f6ef6de.js","static/chunks/pages/index-8d5de16d0c2ecd0a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/u83imgWsAodS0EWlaY6BQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/527-50c02415f05237a1.js","static/chunks/pages/index-8d5de16d0c2ecd0a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
