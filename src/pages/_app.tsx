ab90a0048560c02a79931118bd8a5ab6b7a4b06d
Uurtsaikh <vvrtsaix.n@gmail.com>
2022-02-07 17:59:46 +0800

    Add redux :rocket:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import store from 'redux/store'
 import 'styles/fonts.css'
 import 'styles/global.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider store={store}>
       <Component {...pageProps} />
       <ToastContainer limit={1} />
-    </>
+    </Provider>
   )
 }
 
