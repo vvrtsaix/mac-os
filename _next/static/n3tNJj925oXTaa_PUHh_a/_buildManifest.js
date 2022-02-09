d7bd6e911ca37fbca606191ba26768dfa5c34230
vvrtsaix <vvrtsaix@users.noreply.github.com>
2022-02-09 06:14:53 +0000

    Deploying to gh-pages from @ vvrtsaix/mac-os@1e3833655dc02f2ce7761e8b94f6389eccf6a76d 🚀

---
+++ _next/static/n3tNJj925oXTaa_PUHh_a/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/440-2adf975c76016056.js","static/chunks/pages/index-215e20a4e5fc9708.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
